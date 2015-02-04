b4170031abcdaf70140e8b2cacca1cf66cfb96f0
Romain Pellerin <contact@romainpellerin.eu>
2015-02-04 19:23:23 +0100

    Version 6

--- src/eu/romainpellerin/smsreader/broadcastreceivers/HeadsetPlugReceiver.java
+++ src/eu/romainpellerin/smsreader/broadcastreceivers/HeadsetPlugReceiver.java
@@ -19,11 +19,11 @@ public class HeadsetPlugReceiver extends BroadcastReceiver {
             switch(state) {
                 case(0):
                     Log.d("ok", "Headset unplugged");
-                	last_unplugged = System.currentTimeMillis() / 1000;
+                	last_unplugged = System.currentTimeMillis();
                     break;
                 case(1):
                     Log.d("ok", "Headset plugged");
-                	if ((System.currentTimeMillis() / 1000) - 2 <= last_unplugged) // Only if unplugged since more than 2 seconds
+                	if ((System.currentTimeMillis() - 3000) <= last_unplugged) // Only if unplugged since more than 2 seconds
                 		return;
                 	// If no call at the moment, let's play music :)
 	                TelephonyManager telephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
